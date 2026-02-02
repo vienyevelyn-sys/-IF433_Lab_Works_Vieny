a8db78c1e22cb91e966e23ce9c9381e12620ec7d
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-02-02 16:17:42 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_108031_vienyEvelyn/week01/GameStore.kt
+++ src/oop_108031_vienyEvelyn/week01/GameStore.kt
@@ -2,7 +2,15 @@ package oop_108031_vienyEvelyn.week01
 
 fun main(){
     val gameTitle : String = "Where Winds Meet"
-    val price : Int = 10000
+    val price : Int = 100000
+
+    val diskon = calculateDiscount(price)
+    val harga_akir =
+    println("Judul: $gameTitle")
+    println("Harga asli: $price")
+    println("Diskon: $diskon")
+    println("Harga akhir: ${price - (price * diskon/100)}")
 
 }
 fun calculateDiscount(price: Int): Int = if (price > 500000) 20 else 10
+
