fe21fc41987b779a7e0fef7938d27a127765be88
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-02-02 16:02:22 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_108031_vienyEvelyn/week01/CircleCalculator.kt
+++ src/oop_108031_vienyEvelyn/week01/CircleCalculator.kt
@@ -1,6 +1,6 @@
 package oop_108031_vienyEvelyn.week01
 
-fun main(args: Array<String>) {
+fun main() {
 
     val radius: Double = 7.0
     val pi: Double = 3.14
@@ -10,15 +10,5 @@ fun main(args: Array<String>) {
     var result = checkSize(area)
     println("Result: $result")
 }
-
-/*
-fun checkSize(area: Double) {
-    if (area > 100) {
-        println("This is a Big Circle")
-    }
-    else{
-        println("This is a Small Circle")
-    }
-}*/
 
 fun checkSize(area: Double) = if (area > 100) "This is Big Circle" else "This is Small Circle"
