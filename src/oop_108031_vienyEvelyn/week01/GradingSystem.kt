280f9a6e91ccc77239095ff940691e0371340165
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-02-02 15:29:13 +0700

    week01: add calculateStatus function

--- src/oop_108031_vienyEvelyn/week01/GradingSystem.kt
+++ src/oop_108031_vienyEvelyn/week01/GradingSystem.kt
@@ -1,5 +1,7 @@
 package oop_108031_vienyEvelyn.week01
 
+fun calculateStatus(score: Int) = if(score > 75) "lulus" else "tidak lulus"
+
 fun main(){
     var name: String = "Vieny Evelyn"
     var score: Int = 80
@@ -7,11 +9,15 @@ fun main(){
     //println("Nama:" + name + ", Nilai: "+ score)
     //print("Nama: $name, Nilai: $score")
 
-    val grade = when(score){
-        in 90..100 -> "A"
-        in 80..89 -> "B"
-        in 70..79 -> "C"
-        else -> "D"
-    }
-    print("Grade kamu: $grade")
+    //val grade = when(score){
+     //   in 90..100 -> "A"
+     //   in 80..89 -> "B"
+     //   in 70..79 -> "C"
+     //   else -> "D"
+    //}
+    //print("Grade kamu: $grade")
+
+    println("status: ${calculateStatus(score)}")
+
+
 }
