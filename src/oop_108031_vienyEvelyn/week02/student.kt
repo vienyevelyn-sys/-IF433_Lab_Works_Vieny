0c0510ef84ae3f0aa4d668c7361c60d5393abe11
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-02-09 15:21:15 +0700

    week02: add secondary constructor

--- src/oop_108031_vienyEvelyn/week02/student.kt
+++ src/oop_108031_vienyEvelyn/week02/student.kt
@@ -5,6 +5,9 @@ class Student(
     val nim: String,
     var major: String
 ){
+    constructor(name: String, nim: String): this(name, nim, "Non-Matriculated"){
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan)")
+    }
     init{
         if(nim.length != 5){
             println("WARNING: objek tercipta dengan NIM ($nim) yang tidak valid!")
