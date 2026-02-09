0f4b795bf0b29dbd6c5a70370f8f8440050ea2eb
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-02-09 15:10:12 +0700

    week02: add warning validation in init block

--- src/oop_108031_vienyEvelyn/week02/student.kt
+++ src/oop_108031_vienyEvelyn/week02/student.kt
@@ -5,6 +5,15 @@ class Student(
     val nim: String,
     var major: String
 ){
+    init{
+        if(nim.length != 5){
+            println("WARNING: objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem")
 
+        }
+        else{
+            println("LOG: Objek student $name berhasil dialokasikan di Memory")
+        }
+    }
 }
 
