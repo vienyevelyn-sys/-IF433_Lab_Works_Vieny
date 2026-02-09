45bc40c996ff0c17fbb1a1b3acc0682032c78e3a
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-02-09 15:17:53 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_108031_vienyEvelyn/week02/Main.kt
@@ -0,0 +1,29 @@
+package oop_108031_vienyEvelyn.week02
+
+import java.util.Scanner
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if(nim.length != 5){
+        println("ERROR: Pendaftaran dibatalkan. Nim harus 5 karakter.")
+    }
+    else{
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran selesai")
+    }
+}
+
+
