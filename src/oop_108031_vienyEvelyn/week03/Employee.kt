322da4a7240a1c1ea224ebf8be2b11b30218b4a8
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-02-16 14:54:13 +0700

    week03: fix setter using backing field

--- src/oop_108031_vienyEvelyn/week03/Employee.kt
+++ src/oop_108031_vienyEvelyn/week03/Employee.kt
@@ -2,8 +2,17 @@ package oop_108031_vienyEvelyn.week03
 
 class Employee (val name: String){
     var salary: Int = 0
+//        set(value) {
+//            println("Mencova set gaji ke $value");
+//            this.salary = value;
+//        }
         set(value) {
-            println("Mencova set gaji ke $value");
-            this.salary = value;
+            if(value < 0){
+                println("Error: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0
+            }
+            else{
+                field = value
+            }
         }
 }
