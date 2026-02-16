d8c1c708b19dd97f2879ddc1ccdffabe52b823a0
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-02-16 14:51:00 +0700

    week03: simulate stack overflow error in setter

--- src/oop_108031_vienyEvelyn/week03/Employee.kt
+++ src/oop_108031_vienyEvelyn/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_108031_vienyEvelyn.week03
 
 class Employee (val name: String){
     var salary: Int = 0
+        set(value) {
+            println("Mencova set gaji ke $value");
+            this.salary = value;
+        }
 }
---
+++ src/oop_108031_vienyEvelyn/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_108031_vienyEvelyn.week03
+
+fun main(){
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
