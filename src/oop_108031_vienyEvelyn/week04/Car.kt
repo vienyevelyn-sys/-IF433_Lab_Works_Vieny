436b304fc13753365e99ed53809f0196f7a52baf
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-02-23 14:49:47 +0700

    week04: create Car class extending Vehicle

---
+++ src/oop_108031_vienyEvelyn/week04/Car.kt
@@ -0,0 +1,7 @@
+package oop_108031_vienyEvelyn.week04
+
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand){
+    fun openTrunk(){
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+}
