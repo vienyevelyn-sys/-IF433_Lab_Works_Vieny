2a3a24a16ecd53a2ffe57f71bf81f06f8d917c8d
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-03-02 15:14:19 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_108031_vienyEvelyn/week05/MathHelper.kt
@@ -0,0 +1,15 @@
+package oop_108031_vienyEvelyn.week05
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int{
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int{
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double) : Double{
+        return 3.14 * jariJari * jariJari
+    }
+}
