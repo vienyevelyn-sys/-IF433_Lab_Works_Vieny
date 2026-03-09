782c9c8d76aec4207e8abb22088a3fcb9efc33ab
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-03-09 15:09:20 +0700

    week06: implement decoupled processCheckout in main

---
+++ src/oop_108031_vienyEvelyn/week06/main.kt
@@ -0,0 +1,21 @@
+package oop_108031_vienyEvelyn.week06
+
+fun processCheckout(method: PaymentMethod, amount: Double) {
+    println("--> Memulai checkout...")
+    method.pay(amount)
+}
+
+fun main() {
+    val myWatch = Smartwatch()
+    myWatch.showTime()
+
+    val myPhone = Smartphone()
+    myPhone.turnOn()
+
+    val pay1 = Gopay()
+    val pay2 = CreditCard()
+
+    println("\n=== TESTING CHECKOUT ===")
+    processCheckout(method = pay1, amount = 50000.0)
+    processCheckout(method = pay2, amount = 150000.0)
+}
