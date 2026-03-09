514a738c20363ca5b60b0a14747cf9e91aee0f8b
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-03-09 15:05:21 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_108031_vienyEvelyn/week06/GadgetInterface.kt
@@ -0,0 +1,13 @@
+package oop_108031_vienyEvelyn.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
