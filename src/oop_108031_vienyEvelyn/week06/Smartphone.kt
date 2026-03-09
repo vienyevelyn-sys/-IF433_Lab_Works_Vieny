9ca794ff3cc6b73758f48942a18bd92aff546286
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-03-09 15:06:49 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_108031_vienyEvelyn/week06/Smartphone.kt
+++ src/oop_108031_vienyEvelyn/week06/Smartphone.kt
@@ -1,5 +1,9 @@
 package oop_108031_vienyEvelyn.week06
 
-class Smartphone: Camera, Phone{
-
+class Smartphone : Camera, Phone {
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
 }
