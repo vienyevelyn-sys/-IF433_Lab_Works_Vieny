6308fb234e9f83678a8fd9892857c66d4ffb0592
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-03-16 15:31:13 +0700

    week07: create ApiResponse sealed class

--- src/oop_108031_vienyEvelyn/week07/SystemStates.kt
+++ src/oop_108031_vienyEvelyn/week07/SystemStates.kt
@@ -3,3 +3,9 @@ package oop_108031_vienyEvelyn.week07
 enum class AppState{
     STARTING, RUNNING, STOPPED
 }
+
+sealed class ApiResponse{
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val data: String) : ApiResponse()
+    object Loading : ApiResponse()
+}
