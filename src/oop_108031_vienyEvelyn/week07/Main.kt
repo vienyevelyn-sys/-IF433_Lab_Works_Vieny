0fd6f4e261f11c86cad7eb871e55f17921b99eff
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-03-16 15:19:20 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_108031_vienyEvelyn/week07/Main.kt
+++ src/oop_108031_vienyEvelyn/week07/Main.kt
@@ -1,5 +1,12 @@
 package oop_108031_vienyEvelyn.week07
 
 fun main(){
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
 
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
 }
