5fd7cc1d09629bc7ff01fe92ac0257fb9fc9b39e
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-03-16 15:50:14 +0700

    week07: (task) create GameItem data class

--- src/oop_108031_vienyEvelyn/week07/GameModels.kt
+++ src/oop_108031_vienyEvelyn/week07/GameModels.kt
@@ -4,4 +4,4 @@ enum class ItemRarity(val dropChance: Int){
     COMMON(70), UNCOMMON(10), RARE(2), EPIC(5), LEGENDARY(1)
 }
 
-
+data class GameItem( val name: String, val damage: Int, val rarity: ItemRarity)
