1980868368cc79761ce0b75d4abf106426b0c2a4
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-03-16 16:01:13 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_108031_vienyEvelyn/week07/BattleEvent.kt
@@ -0,0 +1,8 @@
+package oop_108031_vienyEvelyn.week07
+
+sealed class BattleState(){
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+    data class LootDropped(val item: GameItem) : BattleState()
+    data class GameOver(val reason: String) : BattleState()
+    object SafeZone : BattleState()
+}
