d7bf1c82ddf85628da89fa3c5525905f4066dd05
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-04-13 15:21:55 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_108031_vienyEvelyn/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_108031_vienyEvelyn.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId){
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
