33f5464cacd22d8e8bebc071ecc5c07d01b47b7a
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-04-20 15:10:45 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_108031_vienyEvelyn/week09/TradingDashboard.kt
@@ -0,0 +1,11 @@
+package oop_108031_vienyEvelyn.week09
+
+fun main(){
+    val tradeHistory  = listOf(
+        TradeLog("A", "Long", 20, 15.5, "CLOSED"),
+        TradeLog("B", "Short", 10, -5.2, "CLOSED"),
+        TradeLog("A", "Short", 50, 45.0, "OPEN"),
+        TradeLog("C", "Long", 15, -12.8, "OPEN"),
+        TradeLog("B", "Long", 25, 8.4, "CLOSED"),
+        TradeLog("A", "Short", 10, -2.5, "CLOSED"),)
+}
