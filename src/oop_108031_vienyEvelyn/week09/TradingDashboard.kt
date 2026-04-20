ec620f37b4b9d6e30d7524cb36a54dddd2ba06ab
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-04-20 15:14:47 +0700

    week09: (task) filter CLOSED trades only

--- src/oop_108031_vienyEvelyn/week09/TradingDashboard.kt
+++ src/oop_108031_vienyEvelyn/week09/TradingDashboard.kt
@@ -8,4 +8,9 @@ fun main(){
         TradeLog("C", "Long", 15, -12.8, "OPEN"),
         TradeLog("B", "Long", 25, 8.4, "CLOSED"),
         TradeLog("A", "Short", 10, -2.5, "CLOSED"),)
+
+    val closedTrades = tradeHistory.filter{it.status == "CLOSED"}
+    closedTrades.forEach { println(closedTrades) }
+
+
 }
