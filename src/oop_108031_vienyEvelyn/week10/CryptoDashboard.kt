691bc201acfa25818e685a66e4914fefd61f8618
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-04-27 15:40:58 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_108031_vienyEvelyn/week10/CryptoDashboard.kt
+++ src/oop_108031_vienyEvelyn/week10/CryptoDashboard.kt
@@ -5,4 +5,6 @@ fun main(){
     coinRepo.add(Coin("BTC", 20.0))
     coinRepo.add(Coin("ETH", 9.0))
     coinRepo.add(Coin("USDT", 15.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
