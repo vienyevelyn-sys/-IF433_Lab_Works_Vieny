1639b287338aa48b14817d9779248de339ebf234
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-04-27 15:24:14 +0700

    week10: implement generic function with return type

--- src/oop_108031_vienyEvelyn/week10/GenericMethods.kt
+++ src/oop_108031_vienyEvelyn/week10/GenericMethods.kt
@@ -3,3 +3,11 @@ package oop_108031_vienyEvelyn.week10
 fun <T> printData(data: T){
     println("Data yang diterima : $data")
 }
+
+fun <T> printData(data: T){
+    println("Data yang diterima : $data")
+}
+
+fun <T> processData(input: T): T{
+    return input
+}
