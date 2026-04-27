203368ca291c75745b25cb299c9fc889b052ccbf
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-04-27 15:40:40 +0700

    week10: (task) populate coin repository

--- src/oop_108031_vienyEvelyn/week10/CryptoDashboard.kt
+++ src/oop_108031_vienyEvelyn/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_108031_vienyEvelyn.week10
 
 fun main(){
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 20.0))
+    coinRepo.add(Coin("ETH", 9.0))
+    coinRepo.add(Coin("USDT", 15.0))
 }
