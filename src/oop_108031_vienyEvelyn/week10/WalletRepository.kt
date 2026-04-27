261b4b85008ceb191ae6899a00758624de1b7596
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-04-27 15:38:07 +0700

    week10: (task) implement search logic in repository

--- src/oop_108031_vienyEvelyn/week10/WalletRepository.kt
+++ src/oop_108031_vienyEvelyn/week10/WalletRepository.kt
@@ -1,9 +1,18 @@
 package oop_108031_vienyEvelyn.week10
 
-class WalletRepository<T> {
+interface HasName{
+    val nama : String
+}
+class WalletRepository<T : Any> {
     private val items = mutableListOf<T>()
 
     fun add(item: T) = items.add(item)
 
     fun getAll(): List<T> = items
+
+    fun search(keyword: String) :List<T>{
+        return items.filter{
+            it is HasName && it.nama.contains(keyword, ignoreCase = true)
+        }
+    }
 }
