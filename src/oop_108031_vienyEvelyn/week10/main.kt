dffcd9769fd397b7cf776984c054666aa79233ed
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-04-27 15:23:15 +0700

    week10: implement PairBox in main function

--- src/oop_108031_vienyEvelyn/week10/main.kt
+++ src/oop_108031_vienyEvelyn/week10/main.kt
@@ -8,4 +8,10 @@ fun main(){
     println("Isi intBox: ${intBox.value}")
     println("Isi stringBox: ${stringBox.value}")
 
+    println("\n=== TEST MULTIPLE PARAMETERS ===")
+    val itemPrice = PairBox("Bitcoin", 65000)
+    println("Aset: ${itemPrice.key}, Harga: ${itemPrice.value} USD")
+
+
+
 }
