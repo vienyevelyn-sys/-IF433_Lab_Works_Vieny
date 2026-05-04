8634e5546529ceb96dc1f2c2d462ff780a586893
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-05-04 14:51:33 +0700

    week11: implement apply for object initialization

--- src/oop_108031_vienyEvelyn/week11/ScopeConfiguration.kt
+++ src/oop_108031_vienyEvelyn/week11/ScopeConfiguration.kt
@@ -1,3 +1,13 @@
 package oop_108031_vienyEvelyn.week11
 
 data class User(var name: String ="", var age: Int = 0)
+
+fun main(){
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "ALex"
+        age = 26
+    }
+
+    println(user)
+}
