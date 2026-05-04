23791710f7eda6e8394caf7ac8bec4f0a48561b4
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-05-04 15:02:14 +0700

    week11: (task) create extension function for SmartDevice

--- src/oop_108031_vienyEvelyn/week11/SmartHomeModels.kt
+++ src/oop_108031_vienyEvelyn/week11/SmartHomeModels.kt
@@ -1,3 +1,12 @@
 package oop_108031_vienyEvelyn.week11
 
 data class SmartDevice(var name: String, var category: String, var isOnline: Boolean = false, var powerLoad: Int = 0)
+
+fun SmartDevice.diagnose(): String{
+    return "[DIAGNOSTIK] $name " +
+            "| Kategori: $category " +
+            "| Status: ${if (isOnline) 
+                "Online" else "Offline"} " +
+            "| Daya: $powerLoad Watt"
+}
+}
