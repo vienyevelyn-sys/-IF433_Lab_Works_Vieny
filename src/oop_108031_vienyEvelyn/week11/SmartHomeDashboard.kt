c2b4b7352edf0de4bddf0dd3cab76f2893ebccef
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-05-04 15:41:49 +0700

    week11: (task) test full smart home configuration pipeline

--- src/oop_108031_vienyEvelyn/week11/SmartHomeDashboard.kt
+++ src/oop_108031_vienyEvelyn/week11/SmartHomeDashboard.kt
@@ -41,4 +41,9 @@ fun main(){
 
     val totalPower = homeDevices.run { sumOf { it.powerLoad } }
     println("Total power: $totalPower")
+
+    homeDevices.forEach {
+        println(it.diagnose())
+    }
+
 }
