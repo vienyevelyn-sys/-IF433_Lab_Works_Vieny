1c782210ccc0c8476c591d9783f0377f888c0ccd
vienyevelyn-sys <vieny.evelyn@student.umn.ac.id>
2026-05-04 14:53:42 +0700

    week11: implement also for logging side effects

--- src/oop_108031_vienyEvelyn/week11/ScopeConfiguration.kt
+++ src/oop_108031_vienyEvelyn/week11/ScopeConfiguration.kt
@@ -10,4 +10,11 @@ fun main(){
     }
 
     println(user)
+
+    println("\n=== TEST ALSO ===")
+    val numbers = mutableListOf(1, 2, 3)
+    numbers.also {
+        println("Log sebelum ditambah")
+    }.add(4)
+    println("Setelah ditambah: $numbers")
 }
