9d6d36600327fd24f8eec12afde2dd6e354ea6bc
vienyevelyn <vieny.evelin@gmail.com>
2026-05-11 13:53:24 +0700

    week12: implement try as expression

--- src/oop_108031_vienyEvelyn/week12/BasicException.kt
+++ src/oop_108031_vienyEvelyn/week12/BasicException.kt
@@ -12,3 +12,17 @@ fun divide(a: Int, b: Int): Int{
         println("Division attempt finished")
     }
 }
+
+fun main() {
+    println("=== TEST TRY AS EXPRESSION ===")
+    val inputString = "123A"
+
+    // try langsung mengembalikan hasil parsing, atau -1 jika gagal
+    val result: Int = try {
+        Integer.parseInt(inputString)
+    } catch (e: NumberFormatException) {
+        -1
+    }
+
+    println("Hasil parsing: $result")
+}
