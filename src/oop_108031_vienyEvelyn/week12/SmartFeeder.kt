84e270c2fba459814c26d4f837ee1ca268d3dc6d
vienyevelyn <vieny.evelin@gmail.com>
2026-05-11 14:06:35 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_108031_vienyEvelyn/week12/SmartFeeder.kt
@@ -0,0 +1,9 @@
+package oop_108031_vienyEvelyn.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int{
+    require(requestedGram > 0){
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+
+
+}
