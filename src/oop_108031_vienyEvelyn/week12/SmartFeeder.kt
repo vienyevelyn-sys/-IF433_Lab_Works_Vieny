489593013971640dfb18db3680c05ee0bbc8aa32
vienyevelyn <vieny.evelin@gmail.com>
2026-05-11 14:07:49 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_108031_vienyEvelyn/week12/SmartFeeder.kt
+++ src/oop_108031_vienyEvelyn/week12/SmartFeeder.kt
@@ -5,5 +5,7 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
         "Porsi kibble harus lebih dari 0 gr"
     }
 
-
+    if (isJammed) {
+        throw DispenserJamException()
+    }
 }
