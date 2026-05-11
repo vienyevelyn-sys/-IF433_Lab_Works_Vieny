e328a9ae4d66994af401c7d1d932ce79bbf66d7e
vienyevelyn <vieny.evelin@gmail.com>
2026-05-11 13:59:21 +0700

    week12: handle Result using getOrElse and recover

--- src/oop_108031_vienyEvelyn/week12/RunCatchingExample.kt
+++ src/oop_108031_vienyEvelyn/week12/RunCatchingExample.kt
@@ -1,11 +1,18 @@
 package oop_108031_vienyEvelyn.week12
 
-package oop_001_johnthor.week12
 
 fun main() {
     println("=== TEST RUNCATCHING ===")
     val result: Result<Int> = runCatching {
         "42X".toInt()
     }
+
+    // Pattern: getOrElse
+    val safeValue = result.getOrElse { -1 }
+    println("Safe Value (getOrElse): $safeValue")
+
+    // Pattern: recover (bisa mengubah tipe kegagalan jadi sukses)
+    val recovered = result.recover { 0 }.getOrNull()
+    println("Recovered Value: $recovered")
 
 }
