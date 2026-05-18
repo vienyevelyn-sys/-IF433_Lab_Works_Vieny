79009205388fcc8ff0dab30a827015a217201c58
vienyevelyn <vieny.evelin@gmail.com>
2026-05-18 13:41:37 +0700

    week13: create save and load list functions

--- src/oop_108031_vienyEvelyn/week13/StudentSerialization.kt
+++ src/oop_108031_vienyEvelyn/week13/StudentSerialization.kt
@@ -10,3 +10,19 @@ fun fromCsv(line: String) : Student {
     val parts = line.split(",")
     return Student(parts[0], parts[1].toInt(), parts[2].toDouble())
 }
+
+fun saveStudents(student: List<Student>, path: String) {
+    File(path).writeText(student.joinToString( "\n"){
+        it.toCsv()
+    })
+}
+
+fun loadStudents(path: String): List<Student> {
+    return  try {
+        File(path).readLines().map { fromCsv(it) }
+    }
+    catch (e: FileNotFoundException){
+        println("Error: File tidak ditemukan")
+        emptyList()
+    }
+}
